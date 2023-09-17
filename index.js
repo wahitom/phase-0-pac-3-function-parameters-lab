eb792528d35c438789bcd3deb52c6475db806a23
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-17 15:00:49 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+function introduction(name){
+    return `Hi, my name is ${name}.`
+}
+console.log(introduction("Aki"))
+console.log(introduction("Samip"))
+
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+console.log(introductionWithLanguage("Aki", "Ember.js"))
+console.log(introductionWithLanguage("Samip", "React"))
+
+function introductionWithLanguageOptional(name, language = "JavaScript" ){
+    
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+console.log(introductionWithLanguageOptional("Gracie"))
+//console.log(introductionWithLanguageOptional("JavaScript"))
+ 
+
+
+ 
